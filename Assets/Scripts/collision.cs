fa830a2f3685fb0c46e463c7f396d7f8a8fee06c
Ashwin <ashwinrjprht@gmail.com>
2022-03-27 12:59:45 +0530

    UI updated

---
+++ Assets/Scripts/collision.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collision : MonoBehaviour
+{
+    public AudioSource _as;
+    public AudioClip collisionSound;
+
+
+
+    private void Start()
+    {
+        _as = GetComponent<AudioSource>();
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            _as.PlayOneShot(collisionSound);
+        }
+    }
+}
